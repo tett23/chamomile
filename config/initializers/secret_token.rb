79ece926c5c539eb29b5ea59b951aefd821f79dc
tett23 <tett23@gmail.com>
2014-02-15 20:10:13 +0900

    railsでひな形作って必要なgemとかを追加

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chamomile::Application.config.secret_key_base = 'e764537f35499453f93efdf515004be937e314491bae07ff43c9fea866820753f55955e92becc81348092bbebe96f1fbe52a8bca4f20a3cd3ce879562562eea7'
