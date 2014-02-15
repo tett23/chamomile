db08bfbe46fbcd4ff6e76fbdcd370000ef63cdc7
tett23 <tett23@gmail.com>
2014-02-15 21:20:41 +0900

    Wikiの作成と一覧

---
+++ app/controllers/wiki_controller.rb
@@ -0,0 +1,26 @@
+class WikiController < ApplicationController
+  def index
+    @wiki = Wiki.where(user_id: current_user.id).all
+  end
+
+  def new
+    @wiki = Wiki.new
+  end
+
+  def create
+    wiki = permited_params
+    wiki[:user_id] = current_user.id
+    @wiki= Wiki.create(wiki)
+
+    if @wiki.persisted?
+      redirect_to wiki_index_path, flash: {success: "#{params[:wiki][:name]}を作成しました"}
+    else
+      render action: :new
+    end
+  end
+
+  private
+  def permited_params
+    params.require(:wiki).permit(:name, :description)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Chamomile::Application.routes.draw do
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
+  resources :wiki
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
