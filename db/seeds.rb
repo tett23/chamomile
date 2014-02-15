c5b55623526300619d5146073f5107d4f83435ee
tett23 <tett23@gmail.com>
2014-02-15 22:11:45 +0900

    seedにテスト用wikiを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,12 @@
-User.create(
+test_user = User.create(
   username: 'tett23',
   provider: 'twitter',
   uid: 32794163
+)
+
+Wiki.create(
+  name: 'テスト用',
+  slug: 'test',
+  description: "test\nfoo\nbar",
+  user_id: test_user.id
 )
