8db05c5b2b4f613e6dfd7c421788df3d3f4c6cb2
tett23 <tett23@gmail.com>
2014-02-16 02:57:14 +0900

    seedにPage追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,9 +4,16 @@ test_user = User.create(
   uid: 32794163
 )
 
-Wiki.create(
+test_wiki = Wiki.create(
   name: 'テスト用',
   slug: 'test',
   description: "test\nfoo\nbar",
   user_id: test_user.id
+)
+
+Page.create(
+  name: 'テスト用',
+  slug: 'index',
+  body: "h1. テスト用\n\n\n",
+  wiki_id: test_wiki.id
 )
